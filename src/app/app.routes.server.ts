b8c140d65c8663c726e69bb8d518df1a654e1052
OmarEssamEldien <68492964+OmarEssamEldien@users.noreply.github.com>
2026-09-16 14:42:19 +0300

    Update app.routes.server.ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -6,6 +6,14 @@ export const serverRoutes: ServerRoute[] = [
     renderMode: RenderMode.Server
   },
   {
+    path: 'checkout',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'checkout/success',
+    renderMode: RenderMode.Client
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
